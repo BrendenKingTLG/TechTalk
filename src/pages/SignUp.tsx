1a9b2ef7ae2c0fffeaf151dfbf1f2ea95f34cd10
BrendenKingTLG <kingbren@oregonstate.edu>
2023-05-24 12:41:12 -0500

    added settings button

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -6,70 +6,164 @@ export const userStatus = auth.currentUser;
 
 const Login = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
 
-  const onLogin = (e: FormEvent) => {
+  const createAcc = (e: FormEvent) => {
     e.preventDefault();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        // Signed in
-        navigate("/jobs");
-        console.log(auth.currentUser !== null);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-      });
+    if (userInfo.password === userInfo.repeatPassword) {
+      createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
+        .then(() => {
+          // Signed in
+          navigate("/jobs");
+          console.log(auth.currentUser !== null);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      return;
+    }
+  };
+
+  const handleChange = (event: any) => {
+    setUserInfo({ ...userInfo, [event.target.id]: event.target.value });
   };
 
   return (
     <>
-      <div className="d-flex justify-content-center flex-column flex-wrap align-content-center">
-        <form className="d-flex flex-column " style={{ width: "80%" }}>
-          <label
-            htmlFor="email-address"
-            style={{ paddingTop: "1em", marginTop: "1em" }}
-          >
-            Email address
-          </label>
-          <input
-            style={{ marginTop: "1em" }}
-            id="email-address"
-            name="email"
-            type="email"
-            required
-            placeholder="Email address"
-            onChange={(e) => setEmail(e.target.value)}
-          />
+      <section className="vh-100" style={{ backgroundColor: "#eee;" }}>
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-12 col-xl-11">
+              <div className="card text-black" style={{ borderRadius: "25px" }}>
+                <div className="card-body p-md-5">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                        Sign up
+                      </p>
 
-          <label
-            htmlFor="password"
-            style={{ paddingTop: "1em", marginTop: "1em" }}
-          >
-            Password
-          </label>
-          <input
-            style={{ marginTop: "1em" }}
-            id="password"
-            name="password"
-            type="password"
-            required
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button
-            onClick={onLogin}
-            style={{ paddingTop: "1em", marginTop: "1em" }}
-          >
-            Create Account
-          </button>
-        </form>
-        <p className="text-sm text-center pt-5">
-          Already have an account? <NavLink to="/signin">Sign in</NavLink>
-        </p>
-      </div>
+                      <form className="mx-1 mx-md-4">
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="text"
+                              id="form3Example1c"
+                              className="form-control"
+                              onChange={(e) => handleChange(e)}
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example1c"
+                            >
+                              Your Name
+                            </label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="email"
+                              id="email"
+                              className="form-control"
+                              onChange={(e) => handleChange(e)}
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example3c"
+                            >
+                              Your Email
+                            </label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              id="password"
+                              className="form-control"
+                              onChange={(e) => handleChange(e)}
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example4c"
+                            >
+                              Password
+                            </label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              id="repeatPassword"
+                              className="form-control"
+                              onChange={(e) => handleChange(e)}
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example4cd"
+                            >
+                              Repeat your password
+                            </label>
+                          </div>
+                        </div>
+
+                        <div className="form-check d-flex justify-content-center mb-5">
+                          <input
+                            className="form-check-input me-2"
+                            type="checkbox"
+                            value=""
+                            id="form2Example3c"
+                          />
+                          <label
+                            className="form-check-label"
+                            htmlFor="form2Example3"
+                          >
+                            I agree all statements in{" "}
+                            <a href="#!">Terms of service</a>
+                          </label>
+                        </div>
+
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button
+                            type="button"
+                            className="btn btn-primary btn-lg"
+                            onClick={(e) => createAcc(e)}
+                          >
+                            Register
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                      <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                        className="img-fluid"
+                        alt="Sample image"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
